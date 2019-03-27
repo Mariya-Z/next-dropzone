18bafa6d799b09bb1c84277d3166cf5af3463539
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-03-27 16:25:35 +0300

    add library

---
+++ projects/next-dropzone/src/lib/next-dropzone.module.ts
@@ -0,0 +1,9 @@
+import {NgModule} from '@angular/core';
+import {NextDropzoneComponent} from './next-dropzone/next-dropzone.component';
+
+@NgModule({
+  declarations: [NextDropzoneComponent],
+  imports: [],
+  exports: [NextDropzoneComponent],
+})
+export class NextDropzoneModule {}
---
+++ projects/next-dropzone/src/lib/next-dropzone/next-dropzone.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'next-dropzone',
+  templateUrl: './next-dropzone.component.html',
+  styleUrls: ['./next-dropzone.component.scss'],
+})
+export class NextDropzoneComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
