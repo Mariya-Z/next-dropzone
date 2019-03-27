165a70686fadc262f4b24fe1e149c471c4c88ad9
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-03-27 19:19:36 +0300

    fix styles

--- projects/next-dropzone/src/lib/next-dropzone/next-dropzone.component.ts
+++ projects/next-dropzone/src/lib/next-dropzone/next-dropzone.component.ts
@@ -1,12 +1,31 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, HostBinding, Output, EventEmitter, ViewChild, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'next-dropzone',
   templateUrl: './next-dropzone.component.html',
   styleUrls: ['./next-dropzone.component.scss'],
 })
-export class NextDropzoneComponent implements OnInit {
-  constructor() {}
+export class NextDropzoneComponent {
+  public _uniqueId: string;
 
-  public ngOnInit() {}
+  @Input() public id: string = this._uniqueId;
+  @Input() public multiple = false;
+  @Input() public accept = '*.*';
+
+  @HostBinding('class.disabled')
+  @Input() public disabled = false;
+
+  @HostBinding()
+  @Input() public tabindex = 0;
+
+  @Output() public filesSelected = new EventEmitter<File[]>();
+
+  @ViewChild('input') public inputFile: ElementRef;
+
+  public onClick(): void {
+    console.log(this.inputFile);
+    // this.inputFile.click();
+    this.inputFile.nativeElement.checked();
+  }
+
 }
