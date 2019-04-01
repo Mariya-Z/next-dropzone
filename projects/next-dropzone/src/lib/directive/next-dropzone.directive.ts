87c10569fa5f1186ac287091d957389b420b20fb
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-04-01 15:31:17 +0300

    remove useless style, set default color to white

--- projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
+++ projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
@@ -4,7 +4,7 @@ import {Directive, HostListener, HostBinding} from '@angular/core';
   selector: '[nextDropzone]',
 })
 export class NextDropzoneDirective {
-  @HostBinding('style.background') public background = '#eee';
+  @HostBinding('style.background') public background = '#fff';
 
   constructor() {}
 
@@ -17,13 +17,13 @@ export class NextDropzoneDirective {
   @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#eee';
+    this.background = '#fff';
   }
 
   @HostListener('drop', ['$event']) public onDrop(evt) {
     evt.preventDefault();
     evt.stopPropagation();
-    this.background = '#eee';
+    this.background = '#fff';
     const files = evt.dataTransfer.files;
     if (files.length > 0) {
     }
