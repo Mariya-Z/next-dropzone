8d3769f6d2a3af1ad1a15933c033aca8881bc936
Mariia Zubkova <Mariia_Zubkova@epam.com>
2019-04-05 12:45:23 +0300

    fix border for lib component

--- projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
+++ projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
@@ -1,5 +1,4 @@
 import {Directive, HostListener, HostBinding, Output, EventEmitter, ElementRef} from '@angular/core';
-// import '../../vars.scss';
 @Directive({
   selector: '[nextDropzone]',
 })
@@ -16,61 +15,56 @@ export class NextDropzoneDirective {
   constructor(private el: ElementRef) {}
 
   @HostListener('window:dragenter', ['$event']) public onDragEnter(evt) {
-    console.log('window:dragenter');
     if (evt.dataTransfer.types[0] === 'Files') {
+      this.el.nativeElement.style.display = 'block';
       this.border = '2px solid';
       this.borderRadius = '4px';
-      this.borderColor = '$blue';
+      this.borderColor = '#0460a9';
     }
   }
 
-  @HostListener('window:drag', ['$event']) public onDragEnd(evt) {
-    // if (evt.dataTransfer.types[0] === 'Files') {
-    console.log('end');
-    evt.preventDefault();
-    evt.stopPropagation();
-    this.background = this.el.nativeElement.background;
-    this.border = this.el.nativeElement.border;
-    this.borderRadius = this.el.nativeElement.borderRadius;
-    // }
-  }
-
-  @HostListener('dragover', ['$event']) public onDragOver(evt) {
-    console.log('dragover');
-    if (evt.dataTransfer.types[0] === 'Files') {
-      evt.preventDefault();
-      evt.stopPropagation();
-      this.border = '2px solid';
-      this.borderRadius = '4px';
-      this.background = '$blue-light';
-      this.borderColor = '$blue';
-    }
-  }
-
-  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
-    console.log('dragleave');
+  @HostListener('window:dragend', ['$event']) public onDragEnd(evt) {
     if (evt.dataTransfer.types[0] === 'Files') {
       evt.preventDefault();
       evt.stopPropagation();
       this.background = this.el.nativeElement.background;
-      // this.border = this.el.nativeElement.border;
+      this.border = this.el.nativeElement.border;
+      this.borderRadius = this.el.nativeElement.borderRadius;
+    }
+  }
+
+  @HostListener('dragover', ['$event']) public onDragOver(evt) {
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.border = '2px solid';
+      this.borderRadius = '4px';
+      this.background = 'rgba(82, 145, 221, 0.3)';
+      this.borderColor = '#0460a9';
+    }
+  }
+
+  @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.background = this.el.nativeElement.background;
     }
   }
 
   @HostListener('drop', ['$event']) public onDrop(evt) {
-    console.log('drop');
-    // if (evt.dataTransfer.types[0] === 'Files') {
-    evt.preventDefault();
-    evt.stopPropagation();
-    this.background = this.el.nativeElement.background;
-    this.border = this.el.nativeElement.border;
-    const files = evt.dataTransfer.files;
-    if (files.length > 0) {
-      Array.from(files).forEach((element: File) => {
-        this.fileToUpload.push(element);
-      });
-      this.filesSelected.emit(this.fileToUpload);
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.background = this.el.nativeElement.background;
+      this.border = this.el.nativeElement.border;
+      const files = evt.dataTransfer.files;
+      if (files.length > 0) {
+        Array.from(files).forEach((element: File) => {
+          this.fileToUpload.push(element);
+        });
+        this.filesSelected.emit(this.fileToUpload);
+      }
     }
-    // }
   }
 }
