1bd161b5674adb2f2db3948ca3796b09cfb74163
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2019-04-05 12:52:24 +0300

    5 add blue border while file draging (#15)
    * add listener for drag event
    
    * add blue border for dropzone when file is dragging
    
    * fix styles for drag event
    
    * add scss vars
    
    * fix border for lib component

--- projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
+++ projects/next-dropzone/src/lib/directive/next-dropzone.directive.ts
@@ -1,45 +1,70 @@
-import {Directive, HostListener, HostBinding, Output, EventEmitter} from '@angular/core';
-
+import {Directive, HostListener, HostBinding, Output, EventEmitter, ElementRef} from '@angular/core';
 @Directive({
   selector: '[nextDropzone]',
 })
 export class NextDropzoneDirective {
-  @HostBinding('style.background') public background = '#fff';
-  @HostBinding('style.border') public border = '2px solid';
-  @HostBinding('style.border-radius') public borderRadius = '4px';
-  @HostBinding('style.border-color') public borderColor = 'transparent';
+  @HostBinding('style.background') public background;
+  @HostBinding('style.border') public border;
+  @HostBinding('style.border-radius') public borderRadius;
+  @HostBinding('style.border-color') public borderColor;
 
   @Output() public filesSelected = new EventEmitter<File[]>();
 
   public fileToUpload: File[] = [];
 
+  constructor(private el: ElementRef) {}
+
+  @HostListener('window:dragenter', ['$event']) public onDragEnter(evt) {
+    if (evt.dataTransfer.types[0] === 'Files') {
+      this.el.nativeElement.style.display = 'block';
+      this.border = '2px solid';
+      this.borderRadius = '4px';
+      this.borderColor = '#0460a9';
+    }
+  }
+
+  @HostListener('window:dragend', ['$event']) public onDragEnd(evt) {
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.background = this.el.nativeElement.background;
+      this.border = this.el.nativeElement.border;
+      this.borderRadius = this.el.nativeElement.borderRadius;
+    }
+  }
+
   @HostListener('dragover', ['$event']) public onDragOver(evt) {
     if (evt.dataTransfer.types[0] === 'Files') {
       evt.preventDefault();
       evt.stopPropagation();
+      this.border = '2px solid';
+      this.borderRadius = '4px';
       this.background = 'rgba(82, 145, 221, 0.3)';
       this.borderColor = '#0460a9';
     }
   }
 
   @HostListener('dragleave', ['$event']) public onDragLeave(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    this.background = '#fff';
-    this.borderColor = 'transparent';
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.background = this.el.nativeElement.background;
+    }
   }
 
   @HostListener('drop', ['$event']) public onDrop(evt) {
-    evt.preventDefault();
-    evt.stopPropagation();
-    this.background = '#fff';
-    this.borderColor = 'transparent';
-    const files = evt.dataTransfer.files;
-    if (files.length > 0) {
-      Array.from(files).forEach((element: File) => {
-        this.fileToUpload.push(element);
-      });
-      this.filesSelected.emit(this.fileToUpload);
+    if (evt.dataTransfer.types[0] === 'Files') {
+      evt.preventDefault();
+      evt.stopPropagation();
+      this.background = this.el.nativeElement.background;
+      this.border = this.el.nativeElement.border;
+      const files = evt.dataTransfer.files;
+      if (files.length > 0) {
+        Array.from(files).forEach((element: File) => {
+          this.fileToUpload.push(element);
+        });
+        this.filesSelected.emit(this.fileToUpload);
+      }
     }
   }
 }
