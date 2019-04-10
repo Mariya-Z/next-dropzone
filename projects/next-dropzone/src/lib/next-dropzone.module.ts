b516760e747b26d1fd5cd9d530c6fe945e220dde
Mariya-Z <35897561+Mariya-Z@users.noreply.github.com>
2019-04-10 17:08:55 +0300

    remove border after file has been dropped (#18)

--- projects/next-dropzone/src/lib/next-dropzone.module.ts
+++ projects/next-dropzone/src/lib/next-dropzone.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {NextDropzoneComponent} from './next-dropzone/next-dropzone.component';
-import { NextDropzoneDirective } from './directive/next-dropzone.directive';
+import {NextDropzoneDirective} from './directive/next-dropzone.directive';
+import {NextDragAndDropService} from './services/next-drag-and-drop.service';
 
 @NgModule({
   declarations: [NextDropzoneComponent, NextDropzoneDirective],
   exports: [NextDropzoneComponent, NextDropzoneDirective],
+  providers: [NextDragAndDropService],
 })
 export class NextDropzoneModule {}
